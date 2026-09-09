f245759af111ec6470df875821ecb7150007cb7e
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-09 09:58:30 -0500

    Update processors_table.sql

--- table/processors_table.sql
+++ table/processors_table.sql
@@ -1,1 +1,11 @@
 
+CREATE DATABASE ProcessorsDB;
+
+USE ProcessorsDB;
+
+CREATE TABLE Processors ( 
+    number INT PRIMARY KEY,
+    name VARCHAR(255),
+    bitsupport INT KEY,
+    platform VARCHAR(255)
+);
